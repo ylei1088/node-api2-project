a07c707188cc50dc2e629e5e4b0a6fff52dcc435
ylei1088 <ylei1088@gmail.com>
2021-09-28 17:19:41 -0500

    mvp

--- api/server.js
+++ api/server.js
@@ -1,2 +1,20 @@
-// implement your server here
-// require your posts router and connect it here
+const express = require("express");
+const postsRouter = require("./posts/posts-router");
+
+const server = express();
+
+server.use(express.json());
+
+server.use("/api/posts", postsRouter);
+
+server.use("*", (req, res) => {
+  res.status(404).json({
+    message: "not found",
+  });
+});
+
+server.get("/", (req, res) => {
+  res.send(`<h2>HI</h2>`);
+});
+
+module.exports = server;
